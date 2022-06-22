99f004069add8c2d0a883e4a6f9746d8a077723c
Trendon Robinson <46141975+TrendonRobinson@users.noreply.github.com>
2022-06-22 19:23:28 -0400

    updates

---
+++ src/StarterPlayer/StarterPlayerScripts/KnitClient.client.lua
@@ -0,0 +1,26 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Load core module:
+local Knit = require(ReplicatedStorage.Packages.Knit)
+local Component = require(ReplicatedStorage.Packages.Component)
+
+Knit.Modules = ReplicatedStorage.Modules
+Knit.Assets = ReplicatedStorage.Assets
+
+
+-- Load all controllers:
+for _, Controller in ipairs(ReplicatedStorage.Controllers:GetDescendants()) do
+	if Controller:IsA("ModuleScript") then
+		require(Controller)
+	end
+end
+
+-- Load all components:
+for _, v in ipairs(ReplicatedStorage.Components:GetDescendants()) do
+	if v:IsA("ModuleScript") then
+		local vModule = require(v)
+	end
+end
+
+-- Start Knit:
+Knit.Start():catch(warn)
